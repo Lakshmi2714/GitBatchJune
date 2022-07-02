2228fc06bd1fdd0cf98d388e1b52f7e1a244ed1c
Lakshmi2714 <lakshmipriya2714@gmail.com>
2022-07-02 15:27:34 +0530

    code done

--- src/org/data/Sample1.java
+++ src/org/data/Sample1.java
@@ -2,6 +2,10 @@ package org.data;
 
 public class Sample1 {
 	
+	private void m1() {
+		System.out.println("Lisha code added");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("work is done");
